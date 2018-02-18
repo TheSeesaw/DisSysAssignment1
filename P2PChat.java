9510ff9c05c531693784cb12ca9685f125547d08
Kris Schindele <ks2455@nau.edu>
2018-02-18 15:45:05 -0700

    base commit

---
+++ P2PChat.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+
+public class P2PChat
+{
+  public static void main(String args[]) throws Exception
+  {
+    // create a server socket
+    // pass the server socket off to the first client (user)
+    // listen for additional connections
+  }
+
+  
+}
