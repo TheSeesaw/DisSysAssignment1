87471028c6efe4038a6015c209091c9fc50a3237
Kris Schindele <ks2455@nau.edu>
2018-02-21 10:23:17 -0700

    new implementation

--- P2PChat.java
+++ P2PChat.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.io.*;
 import java.net.*;
 
@@ -7,45 +8,118 @@ import java.net.*;
  */
 public class P2PChat
 {
+  public static int messageDispatch(String messageType, String payload, String[][] addresses)
+  {
+    int index = 0;
+    InetAddress workingAddress = null;
+    int workingPort = 0;
+    Socket workingSocket = null;
+    PrintWriter outgoing = null;
+    for (;index < addresses.length; index++)
+    {
+      try
+      {
+        workingAddress = InetAddress.getByName(addresses[index][0]);
+        workingPort = Integer.parseInt(addresses[index][1]);
+        workingSocket = new Socket(workingAddress,workingPort);
+        outgoing = new PrintWriter(workingSocket.getOutputStream(), true);
+        // TODO handle other cases
+        outgoing.println(payload);
+      }
+      catch (Exception e)
+      {
+        System.out.println("Failed to send message");
+      }
+    }
+    switch(messageType)
+    {
+      case "0": // connection request
+        break;
+      case "1": // message
+        break;
+      case "2": // disconnection request
+        break;
+      default:
+        System.out.println("Invalid message, please try again.");
+    }
+    return 1;
+  }
+
+  public static String[] getAddressByName(String[][] addresses, String name)
+  {
+    int index = 0;
+    for (; index < addresses.length; index++)
+    {
+      if (name.equals(addresses[index][2]));
+      return addresses[index];
+    }
+    return null;
+  }
+
+  public static boolean handleUserInput(Scanner userIn, String inputStorage, String[][] addresses)
+  {
+    System.out.println("Enter a message:");
+    inputStorage = userIn.nextLine();
+    if (inputStorage.equals("quit"))
+    {
+      return false; // exit case
+    }
+    String messageType = inputStorage.substring(0,1);
+    String protocolCheck = inputStorage.substring(1,2);
+    if (protocolCheck.equals("#") &&
+       (messageType.equals("0") || messageType.equals("1") || messageType.equals("2")))
+    {
+      String payload = inputStorage.substring(2);
+      messageDispatch(messageType, payload, addresses);
+    }
+    else
+    {
+      System.out.println("Invalid message, please try again.");
+    }
+    //System.out.println(messageType);
+    return true;
+  }
+
   public static void main(String args[]) throws Exception
   {
     // create a server socket
+    // NOTE: all of these could be set by command line arguments
     ServerSocket servSock = null;
     Socket userSock = null;
     String hostName = "localhost";
     int portNumber = 8080;
-    DataInputStream dis = null;
-    DataOutputStream dos = null;
-    try {
-        // defaults to port 8080
-        servSock = new ServerSocket(portNumber);
-        // listen for connections
-        // TODO: connection thread will be called here
-        /*
-        NOTE: following code refactored into ConnectionThread
-        while(true){
-            P2PThread uThread;
-            try{
-              userSock = servSock.accept();
-              dis = new DataInputStream(userSock.getInputStream);
-              dos = new DataOutputStream(userSock.getInputStream);
-              // pass the client socket to the thread
-              uThread = new P2PThread(userSock, dis, dos);
-              Thread newParticipant = new Thread(uThread, "P2PThread");
-              newParticipant.start();
-              System.out.println("Client connected.");
-            } catch (IOException e) {
-              System.out.println("Accept failed on port 8080");
-              System.exit(-1);
-            }
-        }
-        */
-
-    } catch (IOException e) {
-        System.out.println("Unable to listen on port 8080");
+    String knownIP = "134.114.109.46";
+    int knownPort = 8081;
+    // initialize known peers with one other address
+    String[][] knownPeers = new String[1][3];
+    knownPeers[0][0] = knownIP;
+    knownPeers[0][1] = Integer.toString(knownPort);
+    knownPeers[0][2] = "A Friend";
+    try
+    {
+      System.out.println("Starting server . . .");
+      servSock = new ServerSocket(portNumber);
+      // listen for connections
+      // TODO: connection thread will be called here
+      // server is now running and listening for connections
+      System.out.println("Server started.");
+    }
+    catch (IOException e)
+    {
+        System.out.println("Unable to start server listening on port: " + Integer.toString(portNumber));
         System.exit(-1);
     }
-
-    // listen for additional connections
+    // TODO create user socket, depending
+    // listen for input from the user
+    Scanner userIn = new Scanner(System.in);
+    String inputStorage = null;
+    //String outputData = null;
+    boolean active = true;
+    System.out.println("Now listening for input. Type quit to exit");
+    while(active)
+    {
+      active = handleUserInput(userIn, inputStorage, knownPeers);
+    }
+    System.out.println("Now Exiting . . .");
   }
 }
